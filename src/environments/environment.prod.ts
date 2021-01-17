a32bc387701cff80d30653627efda71a73a3b22b
berkay <41726535+BerkayCiliz@users.noreply.github.com>
2021-01-18 01:10:34 +0300

    130202013

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+ 
+firebaseConfig : {
+  apiKey: "AIzaSyCB0793-cvzZ6QT0v8AXR1n5-JxP2exm-I",
+  authDomain: "fir-login-41aff.firebaseapp.com",
+  projectId: "fir-login-41aff",
+  storageBucket: "fir-login-41aff.appspot.com",
+  messagingSenderId: "216204343483",
+  appId: "1:216204343483:web:098c44a891bea3fa3e119e"
+}
+};
